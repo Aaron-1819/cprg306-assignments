b5ef6769d0c6cbfcf5e2c45c37e8f5a6be4e1a81
Ang Xie <xieang18@gmail.com>
2023-10-13 00:57:54 -0600

    week4

---
+++ app/week4/page.js
@@ -0,0 +1,13 @@
+import NewEvent from "./new-item.js";
+
+
+export default function Home() {
+    return (
+      <main className="bg-gradient-to-r from-green-200 via-green-300 to-blue-500">
+      <div className=" min-h-screen">
+        <NewEvent />
+      </div>
+      </main>
+    ); 
+  }
+  
